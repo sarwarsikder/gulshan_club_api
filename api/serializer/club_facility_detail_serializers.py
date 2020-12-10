41b1a3f8d6cc50e9f9b6bbed0779be22fb6adc5e
Sarwar <russelaiub08@gmail.com>
2020-12-10 13:48:49 +0600

    images url

--- api/serializer/club_facility_detail_serializers.py
+++ api/serializer/club_facility_detail_serializers.py
@@ -1,8 +1,20 @@
+from django.conf import settings
 from rest_framework import serializers
 from ..models import ClubFacilityDetail
 
 
 class ClubFacilityDetailSerializer(serializers.ModelSerializer):
+    image_medium = serializers.SerializerMethodField('get_image_medium')
+    image_thumbnail = serializers.SerializerMethodField('get_image_thumbnail')
+
+    def get_image_medium(self, obj):
+        if (obj.image_medium):
+            return '%s%s' % (settings.MEDIA_URL, obj.image_medium)
+
+    def get_image_thumbnail(self, obj):
+        if (obj.image_thumbnail):
+            return '%s%s' % (settings.MEDIA_URL, obj.image_thumbnail)
+
     class Meta:
         model = ClubFacilityDetail
         fields = ('id',
--- api/serializer/event_serializers.py
+++ api/serializer/event_serializers.py
@@ -1,8 +1,19 @@
+from django.conf import settings
 from rest_framework import serializers
 from ..models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+    image_medium = serializers.SerializerMethodField('get_image_medium')
+    image_thumbnail = serializers.SerializerMethodField('get_image_thumbnail')
+
+    def get_image_medium(self, obj):
+        if (obj.image_medium):
+            return '%s%s' % (settings.MEDIA_URL, obj.image_medium)
+
+    def get_image_thumbnail(self, obj):
+        if (obj.image_thumbnail):
+            return '%s%s' % (settings.MEDIA_URL, obj.image_thumbnail)
     class Meta:
         model = Event
         fields = ('id',
