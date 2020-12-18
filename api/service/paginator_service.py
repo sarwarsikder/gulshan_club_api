0adb115638fa491bfa5673af2877d419a87b7aec
sarwar_sikder <sarwar@workspaceit.com>
2020-12-18 09:21:57 +0600

    Pagination data set change

--- api/service/paginator_service.py
+++ api/service/paginator_service.py
@@ -15,3 +15,19 @@ def response_paginated(page_obj, data, request):
         'results': data
     }
     return JsonResponse({'status': True, 'data': d})
+
+def response_paginated_user(page_obj, data, request):
+    next_page = None
+    previous_page = None
+    if page_obj.has_previous():
+        previous_page = request.path + "?page=" + str(page_obj.previous_page_number())
+    if page_obj.has_next():
+        next_page = request.path + "?page=" + str(page_obj.next_page_number())
+    d = {
+        'status': True,
+        'next': next_page,
+        'previous': previous_page,
+        'count': page_obj.paginator.count,
+        'results': data
+    }
+    return JsonResponse(d)
