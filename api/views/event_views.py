2c7424a26c2cc9099cae56ab15c456713b1e2f30
sarwar sikder <sarwar@workspaceit.com>
2021-03-24 10:40:42 +0600

    event pagination added

--- api/views/event_views.py
+++ api/views/event_views.py
@@ -2,7 +2,10 @@ from ..models import Event
 from rest_framework import generics, permissions
 from api.serializer.event_serializers import EventSerializer
 from rest_framework.response import Response
+from api.service import paginator_service
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 import datetime
+
 
 
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
@@ -23,9 +26,26 @@ class EventList(generics.ListAPIView):
             print(end_year)
             queryset = Event.objects.all().filter(start_date__range=[start_year,end_year])
             serializer = EventSerializer(queryset, many=True)
+            event_data = EventSerializer(queryset, many=True).data
         else:
             serializer = EventSerializer(queryset, many=True)
-        return Response(serializer.data)
+            event_data = EventSerializer(event_list, many=True).data
+
+        
+
+
+        paginator = Paginator(queryset, 10)
+        page = request.GET.get('page', 1)
+
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            queryset = paginator.page(1)
+        except EmptyPage:
+            queryset = paginator.page(paginator.num_pages)
+
+        return paginator_service.response_paginated_user(queryset, event_data, request)
+
 
 class EventDetails(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
