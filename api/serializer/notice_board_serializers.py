966424236eddde5d956609e546366e310dc0bd1d
sarwar_sikder <sarwar@workspaceit.com>
2021-07-19 09:05:12 +0600

    notice events

--- api/serializer/notice_board_serializers.py
+++ api/serializer/notice_board_serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from django.conf import settings
 from  ..models import NoticeBoard
 
 class NoticeBoardSerializer(serializers.ModelSerializer):
+    image_medium = serializers.SerializerMethodField('get_image_medium')
+    image_thumbnail = serializers.SerializerMethodField('get_image_thumbnail')
+
+    def get_image_medium(self, obj):
+        if (obj.image_medium):
+            return '%s%s' % (settings.MEDIA_URL, obj.image_medium)
+
+    def get_image_thumbnail(self, obj):
+        if (obj.image_thumbnail):
+            return '%s%s' % (settings.MEDIA_URL, obj.image_thumbnail)
     class Meta:
         model = NoticeBoard
         ordering_fields = ['id', 'created_at']
